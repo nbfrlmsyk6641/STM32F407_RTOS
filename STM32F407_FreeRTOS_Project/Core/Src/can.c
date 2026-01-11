a4bacb98a0b823592ef86b1d9f96f05b4a405845
ChanghaoQiu <2368831528@qq.com>
2026-01-11 10:08:51 +0800

    20260111，HAL库模板工程构建完毕，包括基本的定时器、GPIO、CAN。准备开始构建FreeRTOS模板工程。

---
+++ STM32F407_FreeRTOS_Project/Core/Src/can.c
@@ -0,0 +1,196 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    can.c
+  * @brief   This file provides code for the configuration
+  *          of the CAN instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "can.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+CAN_HandleTypeDef hcan1;
+
+/* CAN1 init function */
+void MX_CAN1_Init(void)
+{
+
+  /* USER CODE BEGIN CAN1_Init 0 */
+
+  /* USER CODE END CAN1_Init 0 */
+
+  /* USER CODE BEGIN CAN1_Init 1 */
+
+  /* USER CODE END CAN1_Init 1 */
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 6;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_9TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_4TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = ENABLE;
+  hcan1.Init.AutoWakeUp = ENABLE;
+  hcan1.Init.AutoRetransmission = ENABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN1_Init 2 */
+
+  /* USER CODE END CAN1_Init 2 */
+
+}
+
+void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspInit 0 */
+
+  /* USER CODE END CAN1_MspInit 0 */
+    /* CAN1 clock enable */
+    __HAL_RCC_CAN1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**CAN1 GPIO Configuration
+    PD0     ------> CAN1_RX
+    PD1     ------> CAN1_TX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_CAN1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    /* CAN1 interrupt Init */
+    HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
+  /* USER CODE BEGIN CAN1_MspInit 1 */
+
+  /* USER CODE END CAN1_MspInit 1 */
+  }
+}
+
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
+{
+
+  if(canHandle->Instance==CAN1)
+  {
+  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+
+  /* USER CODE END CAN1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_CAN1_CLK_DISABLE();
+
+    /**CAN1 GPIO Configuration
+    PD0     ------> CAN1_RX
+    PD1     ------> CAN1_TX
+    */
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_0|GPIO_PIN_1);
+
+    /* CAN1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
+  /* USER CODE BEGIN CAN1_MspDeInit 1 */
+
+  /* USER CODE END CAN1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+// 过滤器配置与CAN初始化函数
+void CAN_User_Init(CAN_HandleTypeDef* hcan)
+{
+  CAN_FilterTypeDef  sFilterConfig;
+
+  // 配置过滤器为：掩码模式，接收所有ID (Mask = 0)
+  sFilterConfig.FilterBank = 0;              // 使用过滤器组0
+  sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK; 
+  sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT; 
+  sFilterConfig.FilterIdHigh = 0x0000;       // ID 高16位
+  sFilterConfig.FilterIdLow = 0x0000;        // ID 低16位
+  sFilterConfig.FilterMaskIdHigh = 0x0000;   // 掩码 高16位 (0表示不关心，即接收所有)
+  sFilterConfig.FilterMaskIdLow = 0x0000;    // 掩码 低16位
+  sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0; // 关联到 FIFO0
+  sFilterConfig.FilterActivation = ENABLE;   // 激活过滤器
+  sFilterConfig.SlaveStartFilterBank = 14;   // 从过滤器组开始编号 (对单CAN芯片无意义，但需配置)
+
+  if (HAL_CAN_ConfigFilter(hcan, &sFilterConfig) != HAL_OK)
+  {
+    /* Filter Configuration Error */
+    Error_Handler();
+  }
+
+  // 1. 启动 CAN 模块
+  if (HAL_CAN_Start(hcan) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler();
+  }
+
+  // 2. 开启接收中断 (FIFO0 消息挂起中断)
+  if (HAL_CAN_ActivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+  {
+    /* Notification Error */
+    Error_Handler();
+  }
+}
+
+// 发送测试消息函数
+void CAN_Send_Test(void)
+{
+    CAN_TxHeaderTypeDef TxHeader;
+    uint8_t TxData[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+    uint32_t TxMailbox;
+
+    TxHeader.StdId = 0x321;         // 标准ID
+    TxHeader.RTR = CAN_RTR_DATA;    // 数据帧
+    TxHeader.IDE = CAN_ID_STD;      // 标准ID格式
+    TxHeader.DLC = 8;               // 数据长度 8字节
+    TxHeader.TransmitGlobalTime = DISABLE;
+
+    // 发送消息，TxMailbox 会返回实际使用的邮箱号
+    if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK)
+    {
+        // 发送失败处理
+    }
+}
+
+// CAN中断接收回调函数
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+  CAN_RxHeaderTypeDef RxHeader;
+  uint8_t RxData[8];
+
+  if (hcan->Instance == CAN1)
+  {
+    // 从 FIFO0 获取消息
+    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
+    {
+      // 可以在这里处理数据
+    }
+  }
+}
+
+/* USER CODE END 1 */
