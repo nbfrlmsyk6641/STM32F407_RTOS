fa2a0d26415a319e61512b9536a057126ecf9448
ChanghaoQiu <2368831528@qq.com>
2026-01-23 22:16:43 +0800

    20260123，更新一下仓库结构，开始回顾C语言基础。

---
+++ C_Code/C_Code_Base/01_C_FirstCode.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* C语言的基础打印 */
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ C_Code/C_Code_Base/02_C.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+
+    a = 520;
+    b = 'F';
+    c = 3.14;
+    d = 3.14159;
+
+    printf("%d\n",a);
+    printf("%c\n",b);
+    printf("%f\n",c);
+    printf("%.3f\n",d);
+
+    return 0;
+}
